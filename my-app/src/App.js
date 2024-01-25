47f2439ae9bbd1c669158155759013dcc03951dc
WaterProofComputer <52865588+WaterProofComputer@users.noreply.github.com>
2024-01-25 09:57:21 +0200

    updatu

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Projektit from './Components/Projektit/projektit';
+import Header from './Components/Header/header';
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Projektit></Projektit>
     </div>
   );
--- my-app/src/Components/Header/header.js
+++ my-app/src/Components/Header/header.js
@@ -1,23 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import './header.scss';
-import './bubbles.scss'
-import CircularProgress from '@mui/joy/CircularProgress';
-import Gallery from './gallery';
-import { Grid, LinearProgress } from '@mui/material';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Timeline from '@mui/lab/Timeline';
-import TimelineItem from '@mui/lab/TimelineItem';
-import TimelineSeparator from '@mui/lab/TimelineSeparator';
-import TimelineConnector from '@mui/lab/TimelineConnector';
-import TimelineContent from '@mui/lab/TimelineContent';
-import TimelineDot from '@mui/lab/TimelineDot';
-import TimelineOppositeContent, {
-  timelineOppositeContentClasses,
-} from '@mui/lab/TimelineOppositeContent';
+import './bubbles.scss';
 
 
-function App() {
+function Header() {
   const [text, setText] = useState('');
   const texts = ['Ohjelmistokehittäjä', 'Fullstack Developer'];
   const speed = 50;
@@ -144,3 +130,4 @@ function App() {
     </div>
     );
 }
+export default Header;
