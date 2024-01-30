9ad3d27d0667f86e25639981acb036f9f9068e68
WaterProofComputer <waterproofcomputer@gmail.com>
2024-01-30 13:57:46 +0200

    AAAAAAAAA

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -16,7 +16,6 @@ function App() {
       <Koulutus />
       <Taidot />
       <Projektit />
-      <Kontaktit />
       <Footer />
     </div>
   );
--- my-app/src/Components/Projektit/projektit.js
+++ my-app/src/Components/Projektit/projektit.js
@@ -3,38 +3,59 @@ import './projektit.scss';
 
 const Projektit = () => {
   const [current, setCurrent] = useState(0);
-  const images = [
-    'wizardtower.png',
-    'maalaus.jpg',
-    'maalaus.png',
-    'logo512.png',
+  const projects = [
+    {
+      image: 'wizardtower.png',
+      info: 'Wizard Tower: 2D Peliprojekti, ei valmis.',
+      url: '',
+    },
+    {
+      image: 'maalaus.jpg',
+      info: 'Kotkantien maalaus ja tapetointi: Maalaus sivustot.',
+      url: 'https://geronimo.okol.org/~lesvil/maalaus/',
+    },
+    {
+      image: 'maalaus.png',
+      info: 'AutoHeaven: Nettisivut auto yritykselle.',
+      url: '',
+    },
+    {
+      image: 'logo512.png',
+      info: 'Project 4: Description for project 4.',
+      url: '',
+    },
   ];
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrent((prev) => (prev + 1) % images.length);
+      setCurrent((prev) => (prev + 1) % projects.length);
     }, 5000);
 
     return () => clearInterval(interval);
-  }, [images.length]);
+  }, [projects.length]);
 
   return (
     <div id='galleriamain'>
-    <div className="galleria">
-      {images.map((image, index) => (
-        <img
-          key={index}
-          src={image}
-          alt={`Gallery Image ${index + 1}`}
-          className={index === current ? 'active' : ''}
-          style={{
-            position: 'absolute',
-            transition: '1s',
-            objectFit: 'fill',
-          }}
-        />
-      ))}
-    </div>
+      <div className="galleria">
+        <a
+          href={projects[current].url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="gallery-item"
+        >
+          <img
+            src={projects[current].image}
+            alt={`Gallery Image ${current + 1}`}
+            style={{
+              width: '400px',
+              height: '400px',
+              transition: 'opacity 0.5s ease-in-out',
+              objectFit: 'cover',
+            }}
+          />
+          <div className="project-info">{projects[current].info}</div>
+        </a>
+      </div>
     </div>
   );
 };
